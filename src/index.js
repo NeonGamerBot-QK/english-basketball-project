557fefb2fdca76f38089e7eea1783c6b503c5bf9
Saahil <neon@saahild.com>
2024-01-24 14:38:16 -0500

    work

--- src/index.js
+++ src/index.js
@@ -5,16 +5,66 @@ const slides = require('./slides')
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const password = 'dixionANDsaahilprojectpassword'
+const clients = []
+const basicAuth = require('express-basic-auth')
 let currentKey = null
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.set('views', 'src/views')
 app.set('view engine', 'ejs')
+app.use('/protected/', (req,res,next) => {
+    // console.log("before the auth")
+    next()
+},basicAuth({
+    authorizer: (username, p) => {
+        console.log('called#')
+        return p === password
+    },
+    challenge: true,
+    realm: 'Imb4T3st4pp',
+}), (req,res,next) => {
+    // console.log("after the auth")
+    next()
+})
+app.get('/protected/auth', (req,res) => {
+    res.json(req.auth)
+})
+// list all socket io clients
+app.get('/protected/clients', (req,res) => {
+    console.log(clients)
+    // res.json(io.sockets.clients().connected)
+    res.json({ clients: [...new Set(clients)].map(c => {
+        return {
+            id: c.id,
+            handshake: c.handshake
+        }
+    })})
+})
+app.get('/protected/slides', (req,res) => {
+    res.json(slides)
+})
+app.post('/protected/suspend_socket', (req,res) => {
+    const { id } = req.body
+    const client = clients.find(c => c.id === id)
+    if(client) {
+        client.emit('suspended')
+        client.disconnect()
+        res.json({ status: true })
+    } else {
+        res.json({ status: false })
+    }
+})
+app.get('/protected/game_code', (req,res) => {
+    res.json({ key: currentKey })
+})
+app.post('/protected/next_slide', (req,res) => {
+    io.sockets.emit('next_slide', req.body.slide || 0)
+})
 app.get('/', (req,res) => {
     res.render('index')
 })
-app.get('/admin', (req,res) => {
+app.get('/protected/admin', (req,res) => {
     res.render('admin')
 })
 app.get('/api/gen_key', (req,res) => {
@@ -38,6 +88,7 @@ if(req.query.key === currentKey) {
 
 io.on('connection', (socket) => {
 socket.on('game_code', (code) => {
+    clients.push(socket)
     console.log(currentKey, code)
     if(code === currentKey) {
         // socket.join(code)
@@ -51,6 +102,11 @@ socket.on('game_code', (code) => {
     } else {
         socket.emit('rejected')
     }
+})
+socket.on('disconnect', () => {
+    const index = clients.indexOf(socket)
+    clients.splice(index, 1)
+    // console.log('disconnected')
 })
 })
 server.listen(3000, () => {    
