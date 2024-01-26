f8a68f0f1359a86ded58ee0ad3efd5fc01c1fb83
Saahil <neon@saahild.com>
2024-01-26 11:51:05 -0500

    More questions

--- src/slides.js
+++ src/slides.js
@@ -1,11 +1,37 @@
-const slides = [{
-    title: "Title Slide",
-    questions: [{ 
-        title: 'Question 1 (1 is correct)',
-        options: ["A","B","C","D"],
+const slides = [
+//     {
+//     title: "Title Slide",
+//     questions: [{ 
+//         title: 'Question 1 (1 is correct)',
+//         options: ["A","B","C","D"],
+//         correct: 0
+//     }]
+// },
+{
+    title: "African-American in the NBA",
+    questions: [{
+        title: "When was Chuck Cooper drafted?",
+        options: ["1950", "1961", "1942", "1959"],
         correct: 0
     }]
-}, {
+}, 
+{
+    title: "African-American in the NBA",
+    questions: [{
+        title: "Fill in the blank: Nat _____ Clifton",
+        options: ["Swaetwetar", "Kingston", "Sweetwater", "FireMan"],
+        correct: 2
+    }]
+},
+{
+    title: "African-American in the NBA",
+    questions: [{
+        title: "What team was Earl Lloyd on?",
+        options: ["Washington Capitols", "Boston Celtics", "New York Knicks", "Philadelphia Warriors"],
+        correct: 0
+    }]
+}, 
+ {
     title: "The black five",
     questions: [{
         title: "What was the black five originally?",
